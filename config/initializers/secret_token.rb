986ef8a50a853ab9d5dabd7a988f6dbf664cd987
lfborjas <luis.borjas@escolarea.com>
2012-02-25 00:26:41 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comprafotos::Application.config.secret_token = '439e5ce64bd0606de98be24665a181d04288cc3f59055d5c5cb34400315ea6579590f89fc35e9c327ace41c48962d5621b37e30d1ef481aa14999c10b1495c9a'
